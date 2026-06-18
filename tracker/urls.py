f908829227e72a77ee2ba46d941cadd03131d552
Olinka-frog <alinashamilova2006@gmail.com>
2026-06-18 17:49:17 +0500

    Deleting water

--- tracker/urls.py
+++ tracker/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('setup/', views.setup_profile, name='setup_profile'),
     path('add-water/', views.add_water, name='add_water'),
+    path('delete-water/<int:intake_id>/', views.delete_water, name='delete_water'),
     path('logout/', views.logout_view, name='logout'),
     path('login/', views.login_view, name='login'),  # ← НОВАЯ СТРОЧКА
 ]
